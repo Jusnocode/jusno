f7b40c6ad45e0c417712830c1a58b28fe4efdbc4
Jusnocode <56450815+Jusnocode@users.noreply.github.com>
2019-12-22 06:49:40 -0800

    happy

---
+++ header/header_test.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- <title>idvs</title> -->
+
+<head>
+    <meta charset="UTF-8">
+    <title>Gallary display </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="format-detection" content="telephone=no">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+
+    <link rel="stylesheet" href="../boostrap/maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="../boostrap/ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="../boostrap/cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="../boostrap/maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    
+    <!-- <link rel="shortcut icon" href="img/red.png" type="image/x-icon" /> -->
+    <!-- <link rel="stylesheet" href="css/header.css"> -->
+    <!-- <link rel="stylesheet" type="text/css" href="css\styles.css"> -->
+ 
+     
+
+
+</head>
+<body>
+  <div class="container" >
+  <div  class="row" style="background-color: red;">    
+           <div class="col-md-2">Talavadi Logo</div>
+            <div  class="row">
+    <!-- <h1><a href="../home/index.php">Jusno<span>Talavadi</span></a></h1> -->
+                <a href="../home/index.php"><h6><strong>Home</strong></h6></a>
+                <a href="../newsfeed/newsfeed.php"><h6><strong>News Feed</strong></h6></a>
+                <a href="../gallary/gallary.php"><h6><strong>Gallary</strong></h6></a>
+                <a href="../newsfeed/contact-us.php"><h6><strong>ContactUs</strong></h6></a>
+            </div>
+
+  </div>
+</div>
+</body>
+</html>
