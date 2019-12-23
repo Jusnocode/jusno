7760b3a5eb6ab1e14cad81140343744f4c81f53b
Jusnocode <56450815+Jusnocode@users.noreply.github.com>
2019-12-23 13:08:08 -0800

    Update header_test.php

--- header/header_test.php
+++ header/header_test.php
@@ -20,6 +20,15 @@
     <!-- <link rel="stylesheet" type="text/css" href="css\styles.css"> -->
  
      
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-154891903-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-154891903-1');
+</script>
 
 
 </head>
